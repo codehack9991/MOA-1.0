d0a0fa5a1c1acf7089c91978542634b68a4c40f1
Shreyas Tripathi <shreyas.tripathi.ece16@itbhu.ac.in>
2019-05-30 19:42:03 +0530

    Create TestErrorType.java

---
+++ src/test/java/fast/common/gmdReplay/client/TestErrorType.java
@@ -0,0 +1,15 @@
+package fast.common.gmdReplay.client;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class TestErrorType {
+
+	@Test
+	public void testErrorType() {
+		ErrorType value = ErrorType.Value;
+		assertNotNull(value);
+	}
+
+}
