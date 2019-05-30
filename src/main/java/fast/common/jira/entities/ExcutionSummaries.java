f16b32a2edcda5b2dbc223ae48cc00dc14e60eb5
Shreyas Tripathi <shreyas.tripathi.ece16@itbhu.ac.in>
2019-05-30 18:03:36 +0530

    Create ExcutionSummaries.java

---
+++ src/main/java/fast/common/jira/entities/ExcutionSummaries.java
@@ -0,0 +1,16 @@
+package fast.common.jira.entities;
+
+import java.util.ArrayList;
+
+public class ExecutionSummaries {
+	private ArrayList<Object> executionSummary;
+
+	public ArrayList<Object> getExecutionSummary() {
+		return executionSummary;
+	}
+
+	public void setExecutionSummary(ArrayList<Object> executionSummary) {
+		this.executionSummary = executionSummary;
+	}
+
+}
