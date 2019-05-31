d1828f3c0e405e74ef3e53646c4e59bee98a97d4
Shreyas Tripathi <shreyas.tripathi.ece16@itbhu.ac.in>
2019-05-31 09:17:10 +0530

    Create SMTPReader.java

---
+++ src/test/java/fast/common/mailexchanger/test/SMTPReader.java
@@ -0,0 +1,14 @@
+package fast.common.mailexchanger.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SMTPReader {
+
+	//@Test
+	public void test() throws RuntimeException {
+		throw new RuntimeException("Sorry we do not support !!");
+	}
+
+}
